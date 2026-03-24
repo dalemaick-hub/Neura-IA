192ac78e296c85ef03ae58bd0f0ab9beaec62bc7
dalemaick21 <dalemaick@gmail.com>
2026-03-24 20:34:23 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 // Sustituye por tu URL real que ya encontramos
 const supabaseUrl = 'https://iboteqiyqqljpbkflkew.supabase.co'
 // Usa la llave 'anon' de tu panel de Supabase
-const supabaseKey = 'PEGA_AQUÍ_TU_LLAVE_ANON_PÚBLICA' 
+const supabaseKey = 'sb_publishable_Er-1Gbhua2RowD08tc5TNw_2REHRW68...' // Pega aquí el código completo
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
